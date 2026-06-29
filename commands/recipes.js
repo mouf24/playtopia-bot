ce9d288e6855a641e646321e17b00d20971e1597
Mouf <80823290+mouf24@users.noreply.github.com>
2026-06-30 01:00:42 +0800

    Update recipes.js

--- commands/recipes.js
+++ commands/recipes.js
@@ -7,6 +7,14 @@ const path = require("path");
 
 const { createRecipeEmbed } = require("../utils/embedBuilder");
 
+// Load database
+const items = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, "../database/items.json"),
+        "utf8"
+    )
+);
+
 const {
     createBrowser
 } = require("../utils/browser");
