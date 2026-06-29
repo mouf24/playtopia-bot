7891fbb21f6b6ed43c7a2e8a79fd5348ba9f3a8f
Mouf <80823290+mouf24@users.noreply.github.com>
2026-06-30 01:03:42 +0800

    Update recipes.js

--- commands/recipes.js
+++ commands/recipes.js
@@ -2,19 +2,6 @@ const {
     SlashCommandBuilder
 } = require("discord.js");
 
-const fs = require("fs");
-const path = require("path");
-
-const { createRecipeEmbed } = require("../utils/embedBuilder");
-
-// Load database
-const items = JSON.parse(
-    fs.readFileSync(
-        path.join(__dirname, "../database/items.json"),
-        "utf8"
-    )
-);
-
 const {
     createBrowser
 } = require("../utils/browser");
